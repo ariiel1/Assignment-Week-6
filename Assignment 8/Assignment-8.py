dfc52a58c04909b873dc4524aa6722e99bc61626
ariiel1 <71622992+ariiel1@users.noreply.github.com>
2020-10-30 15:05:01 +0700

    Update Assignment-8.py

--- Assignment 8/Assignment-8.py
+++ Assignment 8/Assignment-8.py
@@ -1,1 +1,26 @@
+names = ("audino bagon baltoy banette bidoof braviary bronzor carracosta charmeleon cresselia croagunk darmanitan deino emboar emolga exeggcute gabite girafarig gulpin haxorus heatmor heatran ivysaur jellicent jumpluff kangaskhan kricketune landorus ledyba loudred lumineon lunatone machamp magnezone mamoswine nosepass petilil pidgeotto pikachu pinsir poliwrath poochyena porygon2 porygonz registeel relicanth remoraid rufflet sableye scolipede scrafty seaking sealeo silcoon simisear snivy snorlax spoink starly tirtouga trapinch treecko tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask")
+pokelist = names.split(" ")
+pokedict = {}
+for i in pokelist:
+    if i[0] not in pokedict:
+        pokedict[i[0]] = [i]
+    else:
+        pokedict[i[0]].append(i)
 
+def pokemonlist(x):
+    global longest
+
+    longest = []
+    if len(x) > len(longest):
+        longest = x
+
+    if x[-1][-1] in pokedict:
+        for name in pokedict[x[-1][-1]]:
+            if name not in x:
+                x.append(name)
+                pokemonlist(x) 
+
+for x in pokelist:
+    pokemonlist([x])
+
+print(longest)
